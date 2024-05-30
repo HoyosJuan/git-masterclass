c1956bfe8d90c54f38b06c62f1fcb91b57c8c92d
Juan Hoyos <juan.hoyos4@gmail.com>
2024-05-30 13:31:26 -0500

    feat: messages updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 console.log("Hello world!")
 console.log("This feature is awesome!")
-console.log("This feature is awesome!")
-console.log("This feature is awesome!")
+console.log("Second original message!")
+console.log("Another cool message in here!")
